2e09b7f62a57b04e07068b668a6caf238be75cee
jackcooper <hibjack@icloud.com>
2022-03-03 12:57:46 -0600

    Completed Day 33 - Project 7, Part 1
    Created the basic UI
    	Learned about UITabBarController
    Learned about parsing JSON using the Codable protocol

---
+++ Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Jack Cooper on 3/3/22.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Jack Cooper on 3/3/22.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
