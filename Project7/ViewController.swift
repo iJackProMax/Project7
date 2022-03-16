e6ee5d2ffdc9c56cf927c60f8554501939df966a
jackcooper <hibjack@icloud.com>
2022-03-16 18:00:14 -0500

    Completed Challenge 1,2, and 3 of Day 35 Project 7. I added a credits button that describes where the information in the app is coming from. I then created a filter for the user to filter through the petitions based on a string they type in.

--- Project7/ViewController.swift
+++ Project7/ViewController.swift
@@ -9,18 +9,22 @@ import UIKit
 
 class ViewController: UITableViewController {
     var petitions = [Petition]()
+    var filteredPetitions = [Petition]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let urlString: String
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .bookmarks, target: self, action: #selector(displayCredits))
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(promptForFilter))
         
         if navigationController?.tabBarItem.tag == 0 {
             urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
         } else {
             urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
         }
-        
         
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
@@ -42,17 +46,18 @@ class ViewController: UITableViewController {
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
+            filteredPetitions = petitions
             tableView.reloadData()
         }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        return filteredPetitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let petition = petitions[indexPath.row]
+        let petition = filteredPetitions[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
@@ -60,9 +65,41 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailViewController()
-        vc.detailItem = petitions[indexPath.row]
+        vc.detailItem = filteredPetitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
-
+    
+    @objc func displayCredits() {
+        let ac = UIAlertController(title: "Credits", message: "This data comes from the 'We The People' API of the Whitehouse", preferredStyle: .alert)
+        let closeAction = UIAlertAction(title: "Close", style: .default)
+        ac.addAction(closeAction)
+        present(ac, animated: true)
+    }
+    
+    @objc func promptForFilter() {
+        let ac = UIAlertController(title: "Filter", message: "Enter Your Filter", preferredStyle: .alert)
+        ac.addTextField()
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default)
+        ac.addAction(cancelAction)
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] _ in
+            guard let filter = ac?.textFields?[0].text else { return }
+            self?.submitFilter(filter)
+        }
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submitFilter(_ filter: String) {
+        filteredPetitions.removeAll()
+        for petition in petitions {
+            if petition.title.lowercased().contains(filter) || petition.body.lowercased().contains(filter) {
+                filteredPetitions.append(petition)
+            } else if filter == "" {
+                filteredPetitions = petitions
+                tableView.reloadData()
+            }
+            tableView.reloadData()
+        }
+    }
 }
-
